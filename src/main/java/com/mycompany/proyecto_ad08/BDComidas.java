fd23a1334de8e5707d3175e84e90167d2853f17f
inakiRomanos <80259560+inakiRomanos@users.noreply.github.com>
2022-04-15 00:13:33 +0200

    Solo un fallo

---
+++ src/main/java/com/mycompany/proyecto_ad08/BDComidas.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_ad08;
+
+/**
+ *
+ * @author i_rom
+ * Esta clase crea y gestiona una BD MySQL
+ */
+public class BDComidas {
+    
+}
