e486b6f21eecc82eef7325ee7d4f820d51048a5c
inakiRomanos <80259560+inakiRomanos@users.noreply.github.com>
2022-04-15 11:40:20 +0200

    Base de datos Alimentos Lista. Busqueda alimentos. Añadir Alimento (Soluccionar error cuando metes un alimento ya existente

--- src/main/java/com/mycompany/proyecto_ad08/InsertarAlimentos.java
+++ src/main/java/com/mycompany/proyecto_ad08/InsertarAlimentos.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.proyecto_ad08;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author i_rom
  */
 public class InsertarAlimentos {
-    
+
+    public InsertarAlimentos() {
+    }
+
+    public static void insertarAli() {
+
+        if (PanelPrincipal.nombreAlimento_in.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "El campo NOMBRE es obligatorio");
+        } else if (PanelPrincipal.caloriasAlimento_in.equals(0)) {
+            JOptionPane.showMessageDialog(null, "El campo NACIONALIDAD es obligatorio");
+        } else {
+
+            String nombreNuevo = PanelPrincipal.nombreAlimento_in.getText();
+            int caloriasNuevas = (int) PanelPrincipal.caloriasAlimento_in.getValue();
+
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("objectdb:db/database.inakiRomanos");
+            EntityManager em = emf.createEntityManager();
+
+            TypedQuery<Alimentos> querybis = em.createQuery("SELECT d FROM Alimentos d WHERE d.nombre = '" + PanelPrincipal.nombreAlimento_in.getText().toUpperCase() + "'", Alimentos.class);
+            List<Alimentos> comprobarNombre = querybis.getResultList();
+            System.out.println(comprobarNombre);
+
+            if (!comprobarNombre.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "El alimento introducido ya existe");
+
+            } else {
+
+
+                Alimentos nueva = new Alimentos(nombreNuevo, caloriasNuevas);
+
+                em.getTransaction().begin();
+                em.persist(nueva);
+                em.getTransaction().commit();
+
+                em.close();
+                emf.close();
+
+            }
+        }
+
+    }
+
 }
