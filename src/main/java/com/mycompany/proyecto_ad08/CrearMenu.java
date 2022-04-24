c6dd78d15956bfdf395a688deac0b6121f6ee839
inakiRomanos <80259560+inakiRomanos@users.noreply.github.com>
2022-04-24 02:44:16 +0200

    Base de datos Alimentos Lista. Busqueda alimentos. Añadir Alimento (Soluccionar error cuando metes un alimento ya existente Cargar datos tabla menu Conexion Comidas OK Guardamos menU Recargar MenBorrar Alimento Borrar Comida Borrar Alimentos Implementar Calendario

--- src/main/java/com/mycompany/proyecto_ad08/CrearMenu.java
+++ src/main/java/com/mycompany/proyecto_ad08/CrearMenu.java
@@ -1,6 +1,7 @@
 package com.mycompany.proyecto_ad08;
 
 import static com.mycompany.proyecto_ad08.BDAlimentos.modelo;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
@@ -20,6 +21,7 @@ public class CrearMenu {
     }
 
     public static void crearModelo() {
+
         modeloMenu.addColumn("NOMBRE");
         modeloMenu.addColumn("CALORIAS");
         PanelPrincipal.tablaMenu.setModel(modeloMenu);
@@ -29,18 +31,32 @@ public class CrearMenu {
     public static void ponerAlimento() {
         String nombre = (String) CargarTablaAlimentos.modeloAlimentos.getValueAt(PanelPrincipal.tablaAlimentos.getSelectedRow(), 0);
         int calorias = (int) CargarTablaAlimentos.modeloAlimentos.getValueAt(PanelPrincipal.tablaAlimentos.getSelectedRow(), 1);
-        Object[] fila = new Object[]{nombre, calorias};
-        modeloMenu.addRow(fila);
-        PanelPrincipal.tablaMenu.setModel(modeloMenu);
-    }
-    
-    public static void nuevoMenu(){
-    int filas = PanelPrincipal.tablaMenu.getRowCount();
-        for (int i = 0; i < filas; i++) {
-            CrearMenu.modeloMenu.removeRow(0);          
+
+        int validar = 0;
+
+        for (int i = 0; i < PanelPrincipal.tablaMenu.getRowCount(); i++) {
+            if (PanelPrincipal.tablaMenu.getValueAt(i, 0).toString().equals(nombre)) {
+                validar = validar + 1;
+            }
         }
-}
-    public static void quitarAlimento(){
-         modeloMenu.removeRow(PanelPrincipal.tablaMenu.getSelectedRowCount());
+
+        if (validar > 0) {
+            JOptionPane.showMessageDialog(null, "El alimento ya se encuentra en el menu");
+        } else {
+            Object[] fila = new Object[]{nombre, calorias};
+            modeloMenu.addRow(fila);
+            PanelPrincipal.tablaMenu.setModel(modeloMenu);
+        }
+    }
+
+    public static void nuevoMenu() {
+        int filas = PanelPrincipal.tablaMenu.getRowCount();
+        for (int i = 0; i < filas; i++) {
+            CrearMenu.modeloMenu.removeRow(0);
+        }
+    }
+
+    public static void quitarAlimento() {
+        modeloMenu.removeRow(PanelPrincipal.tablaMenu.getSelectedRowCount());
     }
 }
