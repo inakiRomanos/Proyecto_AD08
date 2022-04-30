4e2c1075d361184ff71e386416e9399e591655ca
inakiRomanos <80259560+inakiRomanos@users.noreply.github.com>
2022-04-30 02:32:24 +0200

    Base de datos Alimentos Lista. Busqueda alimentos. Añadir Alimento (Soluccionar error cuando metes un alimento ya existente Cargar datos tabla menu Conexion Comidas OK Guardamos menU Recargar MenBorrar Alimento Borrar Comida Borrar Alimentos Implementar Calendario

--- src/main/java/com/mycompany/proyecto_ad08/AnadirQuitarComida.java
+++ src/main/java/com/mycompany/proyecto_ad08/AnadirQuitarComida.java
@@ -10,7 +10,7 @@ import javax.swing.JOptionPane;
 /**
  *
  * En esta clase podemos añadir, quitar y actualizar menus(en codigo comida)
- * 
+ *
  * @author i_rom
  */
 public class AnadirQuitarComida {
@@ -21,23 +21,33 @@ public class AnadirQuitarComida {
     public static void anadir() throws SQLException {
         int filas = PanelPrincipal.tablaMenu.getRowCount();
 
-        con = getConnection();
+        //Comprobamos que los campos de nombre y calorias tengan datos
+        if (PanelPrincipal.anadirComida.getSelectedItem().toString().equals("")) {
+            JOptionPane.showMessageDialog(null, "El campo TURNO es obligatorio");
+        } else if (PanelPrincipal.fechaAndirComida.getDate() == null) {
+            JOptionPane.showMessageDialog(null, "El campo FECHA es obligatorio");
+        } else if (PanelPrincipal.tablaMenu.getRowCount() == 0) {
+            JOptionPane.showMessageDialog(null, "El menu no contien platos");
+        } else {
 
-        for (int i = 0; i < filas; i++) {
+            con = getConnection();
 
-            PreparedStatement pstmt = null;
-            pstmt = con.prepareStatement("INSERT INTO ALIMENTOS (FECHA,COMIDA,ALIMENTO,CALORIAS) VALUES (?,?,?,?)");
+            for (int i = 0; i < filas; i++) {
 
-            pstmt.setObject(1, PanelPrincipal.fechaAndirComida.getDate());
-            pstmt.setObject(2, PanelPrincipal.anadirComida.getSelectedItem().toString());
-            pstmt.setObject(3, PanelPrincipal.tablaMenu.getValueAt(i, 0));
-            pstmt.setObject(4, PanelPrincipal.tablaMenu.getValueAt(i, 1));
-            pstmt.execute();
+                PreparedStatement pstmt = null;
+                pstmt = con.prepareStatement("INSERT INTO ALIMENTOS (FECHA,COMIDA,ALIMENTO,CALORIAS) VALUES (?,?,?,?)");
+
+                pstmt.setObject(1, PanelPrincipal.fechaAndirComida.getDate());
+                pstmt.setObject(2, PanelPrincipal.anadirComida.getSelectedItem().toString());
+                pstmt.setObject(3, PanelPrincipal.tablaMenu.getValueAt(i, 0));
+                pstmt.setObject(4, PanelPrincipal.tablaMenu.getValueAt(i, 1));
+                pstmt.execute();
+            }
+
+            BDComidas.cargarTabla();
+
+            con.close();
         }
-
-        BDComidas.cargarTabla();
-
-        con.close();
     }
 
     public static void quitar() throws SQLException {
@@ -45,26 +55,26 @@ public class AnadirQuitarComida {
         //Pedimos verificacion del borrado
         int reply = JOptionPane.showConfirmDialog(null, "¿Esta seguro que quiere eliminar el menu seleccionada?", "Confirmar", JOptionPane.YES_NO_OPTION);
         if (reply == JOptionPane.YES_OPTION) {
-            
+
             //Conectamos BBDD
             con = getConnection();
 
             PreparedStatement pstmt = null;
-            
+
             //Borramos la comida seleccionada
             pstmt = con.prepareStatement("DELETE FROM ALIMENTOS WHERE FECHA=? AND COMIDA=?");
 
             pstmt.setObject(1, (Date) BDComidas.modeloComidas.getValueAt(PanelPrincipal.tablaComida.getSelectedRow(), 0));
             pstmt.setObject(2, (String) BDComidas.modeloComidas.getValueAt(PanelPrincipal.tablaComida.getSelectedRow(), 1));
             pstmt.executeUpdate();
-            
+
             //Informamos del borrado
             JOptionPane.showMessageDialog(null, "El menú seleccionada ha sido borrado");
-            
+
             //Actualizamos el panel
             BDComidas.cargarTabla();
             CrearMenu.nuevoMenu();
- 
+
             //Desconectamos BBDD
             con.close();
         }
@@ -72,9 +82,8 @@ public class AnadirQuitarComida {
     }
 
     static public void borrarActualizar() throws SQLException {
-        
-        //En este metodo borramos los datos de la comida seleccionada para poder volver a insertarla con los nuevos datos actualizados
 
+        //En este metodo borramos los datos de la comida seleccionada para poder volver a insertarla con los nuevos datos actualizados
         con = getConnection();
 
         PreparedStatement pstmt = null;
@@ -92,7 +101,6 @@ public class AnadirQuitarComida {
     public static void anadirActualizar() throws SQLException {
 
         //En este metodo volvemos añadir la comida seleccionada con los datos actualizamos
-        
         con = getConnection();
 
         int filas = PanelPrincipal.tablaMenu.getRowCount();
@@ -114,9 +122,8 @@ public class AnadirQuitarComida {
     }
 
     public static void actualizarComida() throws SQLException {
-        
-        //Actulizamos la comida selccionada, borramos los datos viejos e insertamos los nuevos
 
+        //Actulizamos la comida selccionada, borramos los datos viejos e insertamos los nuevos
         borrarActualizar();
         anadirActualizar();
 
